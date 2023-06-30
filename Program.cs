15c6c468c64c9a1e57b374ec20d86a13b8d9b4f2
CommanderJALEN <129650051+CommanderJALEN@users.noreply.github.com>
2023-06-30 23:01:40 +0800

    Added  more methods

--- Program.cs
+++ Program.cs
@@ -27,20 +27,8 @@ namespace WeatherJournal
                         Console.WriteLine("Enter weather information:");
 
                         string? date = GetValidDate();
+                        double temperature = GetValidTemperature();
 
-                        double temperature;
-                        while (true)
-                        {
-                            Console.Write("Temperature (in degrees Celsius):");
-                            if (double.TryParse(Console.ReadLine(), out temperature))
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                Console.Write("Invalid temperature. Please enter a valid number:");
-                            }
-                        }
                         Console.Write("Weather condition: ");
                         string? condition = Console.ReadLine();
 
@@ -53,12 +41,7 @@ namespace WeatherJournal
                         break;
 
                     case "2":
-                        Console.WriteLine("Weather journal entries:");
-                        for (int i = 0; i < dates.Count; i++)
-                        {
-                            Console.WriteLine($"Date: {dates[i]}, Temperature: {temperatures[i]}C, Condition: {conditions[i]}");
-                        }
-                        Console.WriteLine();
+                        DisplayWeatherEntries(dates, temperatures, conditions);
                         break;
 
                     case "0":
@@ -72,23 +55,49 @@ namespace WeatherJournal
                 }
             }
         }
-        
+
         static string? GetValidDate()
         {
             while (true)
             {
-                Console.Write("Date(DD-MM-YYYY):");
+                Console.Write("Date (DD-MM-YYYY): ");
                 string? date = Console.ReadLine();
-                
-                if (DateTime.TryParseExact(date, "dd-mm-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime result))
+
+                if (DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime result))
                 {
                     return date;
                 }
                 else
                 {
-                Console.WriteLine("Invalid date format. Please enter a valid date (DD-MM-YYYY).");
+                    Console.WriteLine("Invalid date format. Please enter a valid date (DD-MM-YYYY).");
                 }
             }
+        }
+
+        static double GetValidTemperature()
+        {
+            while (true)
+            {
+                Console.Write("Temperature (in degrees Celsius): ");
+                if (double.TryParse(Console.ReadLine(), out double temperature))
+                {
+                    return temperature;
+                }
+                else
+                {
+                    Console.Write("Invalid temperature. Please enter a valid number: ");
+                }
+            }
+        }
+
+        static void DisplayWeatherEntries(List<string?> dates, List<double> temperatures, List<string?> conditions)
+        {
+            Console.WriteLine("Weather journal entries:");
+            for (int i = 0; i < dates.Count; i++)
+            {
+                Console.WriteLine($"Date: {dates[i]}, Temperature: {temperatures[i]}C, Condition: {conditions[i]}");
+            }
+            Console.WriteLine();
         }
     }
 }
